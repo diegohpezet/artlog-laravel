1c8884497e00c8661383e649658296f7121143ff
diegohpezet <diegohpezet@gmail.com>
2025-01-17 15:15:49 -0300

    comment factory & seeder

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    use HasFactory, HasUuids;
+
     protected $fillable = [
         'user_id',
         'post_id',
